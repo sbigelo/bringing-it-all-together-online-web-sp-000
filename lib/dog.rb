15c3b2a90bd6340b74491999a9606f41772cc68b
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-30 16:44:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,26 +35,23 @@ attr_accessor :name, :breed, :id
     self.new(id: id, name: name, breed: breed)
   end
 
-  # def self.find_by_name(name)
-  #   sql = <<-SQL
-  #   SELECT * FROM dogs WHERE name = ? LIMIT 1
-  #   SQL
-  #   DB[:conn].execute(sql, name).map do |row|
-  #     self.new_from_db(row)
-  #   end.first
-  # end
   def self.find_by_name(name)
-     sql = <<-SQL
-       SELECT *
-       FROM dogs
-       WHERE name = ?
-       LIMIT 1
-     SQL
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
-     DB[:conn].execute(sql,name).map do |row|
-       self.new_from_db(row)
-     end.first
-   end
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 
 end
